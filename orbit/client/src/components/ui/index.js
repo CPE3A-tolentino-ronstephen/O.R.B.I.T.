85d471510776d2faf73f5ad86c38ba14a96cf619
Ron Stephen Tolentino <2023105911@ms.bulsu.edu.ph>
2026-04-21 14:51:50 +0800

    Revert "source files for web framework"
    This reverts commit 25ad7097fac75e9648ebcc3fe7d96dd06f48af7d.

--- orbit/client/src/components/ui/index.js
+++
@@ -1,7 +0,0 @@
-export { default as StatCard }      from "./StatCard";
-export { default as RiskBadge }     from "./RiskBadge";
-export { default as CountryDetail } from "./CountryDetail";
-export { default as CountryTable }  from "./CountryTable";
-export { default as DiseaseTabs }   from "./DiseaseTabs";
-export { ALL_DISEASES }             from "./DiseaseTabs";
-export { SkeletonBlock, SkeletonRows, SkeletonChart } from "./Skeleton";
