81a34e8fea36d5f50523d83f13a1ab18781084c0
Ron Stephen Tolentino <2023105911@ms.bulsu.edu.ph>
2026-04-22 16:36:17 +0800

    Update DashboardPage.jsx

--- orbit/client/src/pages/DashboardPage.jsx
+++ orbit/client/src/pages/DashboardPage.jsx
@@ -1,5 +1,6 @@
 import { useState, useMemo }  from "react";
 import { useNavigate }        from "react-router-dom";
+import { useAuth }            from "../context/AuthContext";
 import { useDisease }         from "../hooks/useDisease";
 
 const DISEASES = [
